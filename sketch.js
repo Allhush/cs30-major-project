ae21690de9d67ba707f742372d181ab905a5bc21
Allhush <hushagenalliam@gmail.com>
2024-05-01 13:30:27 -0600

    Added P5js templated

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Major Project
+// Alliam Hushagen
+// Wednesday May 1st 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  square(mouseX, mouseY, 100);
+}

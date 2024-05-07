3b004087ba8b352e921f3c322a02b78432c37222
Allhush <hushagenalliam@gmail.com>
2024-05-07 13:31:54 -0600

    Started adding object classes

--- proposal.md
+++ proposal.md
@@ -1,7 +1,7 @@
 # Major project proposal
 
 ## Description
-- Wave defense game, you recruit troops to combat hordes of enemies that come at you in waves becoming progressively more difficult.
+- Wave defense game, you buy troops to combat hordes of enemies that come at you in waves becoming progressively more difficult.
 
 ## Needs to have
 - Money collected from enemies to buy troops
--- sketch.js
+++ sketch.js
@@ -5,6 +5,55 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class SwordTroop{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.health = 100;
+    this.damage = 50;
+    this.height = 5;
+    this.width = 3;
+    this.cost = 20;
+    this.colour = "black";
+  }
+
+  display(){
+    noStroke();
+    fill(this.colour);
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  // attackEnemy(){
+
+  // }
+}
+
+class Zombie{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.health = 50;
+    this.damage = 10;
+    this.speed = 20;
+    this.height = 5;
+    this.width = 3;
+    this.colour = "green";
+    this.coin = 5;
+  }
+
+  display(){
+    noStroke();
+    fill(this.colour);
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  // attackTroops(){
+
+  // }
+
+}
+
+let theTroops = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,4 +62,14 @@ function setup() {
 function draw() {
   background(220);
   
+}
+
+function mousePressed(){
+  if(keyIsDown(90)){
+    let someTroop = new SwordTroop(mouseX, mouseY);
+    theTroops.push(someTroop);
+  }
+  if(keyIsDown(88)){
+    
+  }
 }
